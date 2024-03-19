1b92e11a9584fa63b296c8869f71596da23f529c
ElTala <eltalasnk@gmail.com>
2024-03-18 23:46:39 -0700

    Nuevos modulos

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, Response, request, redirect, url_for
+import requests
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +9,14 @@ def home():
 @app.route('/login')
 def login():
     return render_template('login.html')
+@app.route('/camara',methods=['GET'])
+def test():
+    return render_template('camara.html')
 
+@app.route('/submit',methods=['POST'])
+def submit():
+    image = request.args.get('image')
+    print(type(image))
+    return "Hola"
 if __name__ == '__main__':
     app.run(debug=True)
